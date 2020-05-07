cef8a3aeb560005078787855cccb924264d98b69
jmls9627 <jmls9627@gmail.com>
2020-05-07 15:56:51 -0400

    Organizing my Export with Barrels

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,10 +1,11 @@
-import { EventDetailsComponent } from './events/event-details/event-details.component';
-import { EventsListComponent } from './events/events-list.component';
+import {EventsListComponent, 
+  EventDetailsComponent, 
+  CreateEventComponent,
+  EventRouteActivator,
+  EventListResolver} from "./events/index";
+  
 import { Routes } from "@angular/router";
-import { CreateEventComponent } from "./events/create-event.component";
 import { Error404Component } from './errors/404.component';
-import { EventRouteActivator } from './events/event-details/event-route-activator.service';
-import { EventListResolver } from './events/event-list-resolver.service';
 
 
 export const appRoutes:Routes=[
