9f30073c77f56afd6d57d6c571be377edf135730
jmls9627 <jmls9627@gmail.com>
2020-05-18 13:35:23 -0400

    Content Projection

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -6,9 +6,9 @@ import { IEvent, ISession } from '../share/index';
 @Component({
     templateUrl:"./event-details.component.html",
     styles:[`
-        .container {padding-left: 20px; padding-rigth:20px; }
-        .event-image {height: 100px;}
-        a {cursor:pointer}
+        .container {padding-left:20px; padding-rigth:20px; }
+        .event-image {height:100px;}
+        a {cursor:pointer; }
         `]
 })
 
