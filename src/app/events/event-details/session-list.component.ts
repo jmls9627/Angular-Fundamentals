1e05c73e4b1608a17ff6f2f0ecd608f4f0da5d8c
jmls9627 <jmls9627@gmail.com>
2020-05-20 15:03:12 -0400

    10.Sorting Data

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -11,12 +11,15 @@ import {ISession} from "../share/index"
 
  export class SessionListComponent implements OnChanges{
     @Input() sessions: ISession[];
-    @Input()filterBy: string;
+    @Input() filterBy: string;
+    @Input() sortBy: string;
     visibleSessions:ISession[] = [];
 
     ngOnChanges(){
         if(this.sessions){
             this.filterSessions(this.filterBy);
+            this.sortBy==='name' ? this.visibleSessions.sort(sortByNameAsc)
+            : this.visibleSessions.sort(sortByVotesDesc);
         }
     }
 
@@ -30,4 +33,14 @@ import {ISession} from "../share/index"
         }
 
     }
+ }
+
+ function sortByNameAsc(s1:ISession,s2:ISession){
+     if(s1.name >s2.name) return 1
+     else if(s1.name===s2.name) return 0
+     else return -1
+ }
+
+ function sortByVotesDesc(s1:ISession,s2:ISession){
+    return s2.voters.length-s1.voters.length;
  }
